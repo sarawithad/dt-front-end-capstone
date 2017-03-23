305411e6de09e2f03eed978492426e01db5812da
Dara Thomas <darathomas@Daras-MacBook-Air.local>
2017-03-23 01:43:33 -0500

    more options added

--- app/controllers/BuiltStoryCtrl.js
+++ app/controllers/BuiltStoryCtrl.js
@@ -1,12 +1,9 @@
 "use strict";
-console.log("controller connected");
+console.log("StoryCtrl connected");
 
 app.controller("StoryCtrl", function($scope, $location, StoryFactory, $window, BookshelfFactory, AuthFactory, OneStoryFactory){
 
 	let user = AuthFactory.getUser();
-
-	// let storyBegin;
-	// let storyEnd;
 
 	var component = StoryFactory.getStoryObject();
 
@@ -20,11 +17,9 @@ app.controller("StoryCtrl", function($scope, $location, StoryFactory, $window, B
 	$scope.storyBegin = `Once Upon a Time,`;
 	
 	//builds story with user's chosen story components
-	$scope.storyBody = `there was a ${$scope.storyComponents.chosenMainCharacter} who lived at a ${$scope.storyComponents.chosenPlace}. The ${$scope.storyComponents.chosenMainCharacter}’s best friend was a famous ${$scope.storyComponents.chosenSecondCharacter}. The two became best friends after the ${$scope.storyComponents.chosenSecondCharacter} helped the ${$scope.storyComponents.chosenMainCharacter} defeat the evil villain ${$scope.storyComponents.chosenVillain}, and save the ${$scope.storyComponents.chosenPlace}. The ${$scope.storyComponents.chosenMainCharacter} and ${$scope.storyComponents.chosenSecondCharacter} went everywhere together. They always traveled by way of the ${$scope.storyComponents.chosenMainCharacter}'s ${$scope.storyComponents.chosenTransport}. They especially loved to play all around the ${$scope.storyComponents.chosenPlace} with ${$scope.storyComponents.chosenItem}s and have ${$scope.storyComponents.chosenFood} feasts. The ${$scope.storyComponents.chosenMainCharacter} and the ${$scope.storyComponents.chosenSecondCharacter} were friends forever,`;
+	$scope.storyBody = `there was a ${$scope.storyComponents.chosenMainCharacter} who lived in a ${$scope.storyComponents.chosenPlace}. The ${$scope.storyComponents.chosenMainCharacter}’s best friend was a famous ${$scope.storyComponents.chosenSecondCharacter}. The two became best friends after the ${$scope.storyComponents.chosenSecondCharacter} helped the ${$scope.storyComponents.chosenMainCharacter} defeat the evil villain ${$scope.storyComponents.chosenVillain}, and save the ${$scope.storyComponents.chosenPlace}. The ${$scope.storyComponents.chosenMainCharacter} and ${$scope.storyComponents.chosenSecondCharacter} went everywhere together. They always traveled by way of the ${$scope.storyComponents.chosenMainCharacter}'s ${$scope.storyComponents.chosenTransport}. They especially loved to play all around the ${$scope.storyComponents.chosenPlace} with ${$scope.storyComponents.chosenItem}s and have ${$scope.storyComponents.chosenFood} feasts. The ${$scope.storyComponents.chosenMainCharacter} and the ${$scope.storyComponents.chosenSecondCharacter} were friends forever,`;
 
-	$scope.storyEnd = `The End`;
-
-	// $scope.wholeStory = title + storyBegin + storyBody + storyEnd;
+	$scope.storyEnd = `The End.`;
 
 	console.log("storyBody", $scope.storyBody);
 
